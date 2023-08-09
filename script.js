f700527ae2b384cebdc6d551c0dee321bbb6e498
subhankar2524 <121926051+subhankar2524@users.noreply.github.com>
2023-08-09 18:27:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
+
+let text = document.getElementById('typing');
+let index = 1;
+
+let Name = ["Elevate Your Code with PowerMyCode!","Embark on a Path of Limitless Possibilities"];
+let isCompleted = false;
+let i=0;
+const typeWriter = () => {
+    // console.log(index);
+    if(isCompleted == false){
+        let new_title = Name[i].slice(0,index);
+        text.innerText = new_title;
+        index++;
+    }else{ // decrease
+        let new_title = Name[i].slice(0,index);
+        text.innerText = new_title;
+        index--;
+    }
+    if(index>Name[i].length+10){
+        isCompleted = true;
+    }
+    else if(index==0){
+        isCompleted = false;
+        if(i==Name.length-1){
+            i=0;
+        }
+        i++;
+    }
+    setTimeout( () => {typeWriter()}, 100);
+}
+
+
+typeWriter ();
+const cursorElement = document.getElementById('cursor');
+function toggleCursorVisibility() {
+    cursorElement.style.visibility = cursorElement.style.visibility === 'visible' ? 'hidden' : 'visible';
+}
+setInterval(toggleCursorVisibility, 500); // Change the blinking speed by adjusting the interval time in milliseconds
+
+////////////////////////////////////////////////////////////
+
+let people = document.getElementById("peopleText");
+console.log(people);
+
+function people1(){
+    people.innerHTML = "Arun Rachuri is an Ex-IBMer and has the vision to deliver world class websites and mobile applications meaningful to businesses across the globe. He has a proven track record of building clientele including popular MNC’s. Arun has a Bachelor's Degree in Business Administration.";
+}
+function people2(){
+    people.innerHTML = "Ram Peri, another Ex-IBMer, brings expertise in B2B sales, contract executions, and project budgeting. His background, along with a Bachelor's Degree in World History, enables him to excel in sales and project management.";
+}
+function people3(){
+    people.innerHTML = "Simran Tripathy is the HR Management Specialist overseeing the Human Resources department. With an MBA in Human Resource Management, she ensures a positive and inclusive work environment for the team.";
+}
+
+////////////////////////////////////////////////////////////
 const slides = document.querySelectorAll(".slide")
 // console.log(slides)
 var counter =0;
@@ -20,7 +75,7 @@ const goNext = () => {
         counter=-1;
     }
     counter++;
-    console.log(counter);
+    // console.log(counter);
     slideImage();
     textFetch(counter);
 }
